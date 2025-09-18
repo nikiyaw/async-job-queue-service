96cd60003ce3dd07d5ab427a8d5738768ec64515
Niki Yaw <nikyaw95@gmail.com>
2025-09-18 15:26:04 -0700

    success: tried running uvicorn, redis & celery

--- src/api/routers/jobs.py
+++ src/api/routers/jobs.py
@@ -4,6 +4,7 @@ from ..models.job import JobSubmission # This is the Pydantic model
 from ..core.database import get_db # This is the database dependency
 from ..models.sql_models.job import Job as JobModel # This is the SQLAlchemy model
 from ..core.redis_config import redis_client
+from ...worker.celery_worker import celery_app
 
 router = APIRouter(
     prefix="/jobs",
@@ -24,10 +25,9 @@ def submit_job(job: JobSubmission, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_job) # To get the newly created ID
 
-    # Add the job ID to the Redis queue for processing
-    # The message we're sending is just the job ID
-    redis_client.lpush("job_queue", str(db_job.id))
-    print(f"Job added to Redis queue. Current queue size: {redis_client.llen('job_queue')}")
+    # Add the job to the Celery queue by using a task from Celery app directly, which handles serialization
+    task_name = "src.worker.celery_worker.process_job"
+    celery_app.send_task(task_name, args=[db_job.id], queue="job_queue")
 
-    print(f"Received job of type: {job.job_type} with payload: {job.payload}")
-    return {"message": "Job received successfully", "job_type": job.job_type}
+    print(f"Received job of type: {db_job.job_type} and persisted to DB with ID: {db_job.id}")
+    return {"message": "Job received successfully", "job_id": db_job.id, "job_type": db_job.job_type}
