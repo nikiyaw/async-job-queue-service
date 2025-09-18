c0795c3aff1d383bee8b9e33100a876b4753f620
Niki Yaw <nikyaw95@gmail.com>
2025-09-18 07:55:26 -0700

    update main.py file with new router code

--- src/api/main.py
+++ src/api/main.py
@@ -1,32 +1,15 @@
-from fastapi import FastAPI, HTTPException, status
-from api.models.job import JobSubmission
+from fastapi import FastAPI
+from .routers import jobs
 
-from sqlalchemy.orm import Session
-from src.api.database import SessionLocal, engine, Job, Base
-
-# This line ensures the tables are created when the app starts
-Base.metadata.create_all(bind=engine)
-
-# Dependency to get a database session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
 
 app = FastAPI(
-    title="Job Queue Service"
+    title="Job Queue Service",
+    description="A service for handling asynchronous tasks"
 )
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Job Queue Service!"}
 
-@app.post("/jobs/submit", status_code=status.HTTP_201_CREATED)
-def submit_job(job: JobSubmission):
-    # In Week 2, we will add the logic to store this job in the database
-    # and add it to the message queue. For now, we'll just return a success message.
-    print(f"Received job of type: {job.job_type} with payload: {job.payload}")
-    return {"message": "Job received successfully", "job_type": job.job_type}
+# Include the routers
+app.include_router(jobs.router)
