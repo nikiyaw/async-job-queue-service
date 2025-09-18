0b1d332bb2bcd7454878b064a15bfbbcbffcf177
Niki Yaw <nikyaw95@gmail.com>
2025-09-18 09:25:30 -0700

    update ci.yml to spin up a PostgreSQL container

--- src/api/core/database.py
+++ src/api/core/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
 # Get DB credentials from environment variables (best practice)
 # We'll hardcode them for now since we haven't done docker-compose for the whole stack yet.
 # In Week 3, we'll switch this to use environment variables.
-DATABASE_URL = "postgresql://user:password@localhost:5433/jobs_db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://user:password@localhost:5433/jobs_db")
 
 # SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
