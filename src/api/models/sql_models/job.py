118db2b90c8f25f553cd931194ad76a731bade7e
Niki Yaw <nikyaw95@gmail.com>
2025-09-19 12:28:14 -0700

    update database model to include error_message field

--- src/api/models/sql_models/job.py
+++ src/api/models/sql_models/job.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, JSON, DateTime, Enum
+from sqlalchemy import Column, Integer, String, JSONB, DateTime, Enum
 from sqlalchemy.orm import relationship
 import datetime
 
@@ -10,9 +10,10 @@ class Job(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     job_type = Column(String, nullable=False)
-    payload = Column(JSON, nullable=False)
+    payload = Column(JSONB, nullable=False)
     status = Column(Enum("queued", "processing", "completed", "failed", name="job_status"), default="queued")
     retries = Column(Integer, default=0)
     created_at = Column(DateTime, default=datetime.datetime.now)
     updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
-    result = Column(JSON, nullable=True)
+    result = Column(JSONB, nullable=True)
+    error_message = Column(JSONB, nullable=True)
