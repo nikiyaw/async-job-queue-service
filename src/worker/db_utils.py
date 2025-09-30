ab174b00018d3edf5b68fb41b6e99ef066a944d3
Niki Yaw <nikyaw95@gmail.com>
2025-09-29 19:20:22 -0700

    implemented logging centralization in db_utils.py

--- src/worker/db_utils.py
+++ src/worker/db_utils.py
@@ -1,17 +1,24 @@
-from contextlib import contextmanager
-from ..api.core.database import SessionLocal
+from contextlib import contextmanager 
+from sqlalchemy.orm import Session 
+from src.api.core.database import SessionLocal 
+from src.api.core.logging_config import get_logger 
+
+logger = get_logger(__name__)
 
 @contextmanager
 def get_db_session():
-    """
-    Provides a transactional scope around a series of operation.
+    """ 
+    Provides a database session inside a context manager. Ensures the session is committed if successful, or rolled back on error, and always closed at the end. 
     """
     db = SessionLocal()
     try:
         yield db
         db.commit()
+        logger.debug("Database session committed successfully.")
     except Exception as e:
         db.rollback()
+        logger.exception(f"Error during DB session, rolled back: {e}")
         raise e
     finally: 
         db.close()
+        logger.debug("Database session closed.")
